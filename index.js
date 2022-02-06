7735539cf1f74a7dfbc4a2c8334cf2f3b36030c9
TA2LSM <semihsenol@gmail.com>
2022-02-06 22:18:09 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const url = "https://www.imdb.com/title/tt0088763/reference";
+//const url = "https://www.imdb.com/title/tt0088763/reference"; // Back to the Future
+//const url = "https://www.imdb.com/title/tt0145487/reference"; // Spider-Man
+//const url = "https://www.imdb.com/title/tt9603212/reference"; // Mission: Impossible 7
+const url = "https://www.imdb.com/title/tt2322441/reference"; //Fifty Shades of Grey
 
 const options = {
   method: "GET",
@@ -12,63 +15,107 @@ const options = {
 (async () => {
   try {
     const res = await axios(options);
-    //console.log(res.data);
-
     const $ = cheerio.load(res.data);
-    const movieTitle = $(
+
+    //---------------------------
+    let movieTitle = $(
       `div[class="titlereference-header"] > div > h3[itemprop="name"]`
       //`div.titlereference-header > div:nth-child(1) > h3:nth-child(3)`
     )
       .text()
-      .trim()
-      .replace(/[&\/\\#,+()$~%.'":*?<>{}0-9\n]/g, ""); //istenmeyen karakterleri kaldır
+      .replace(/[\t\r]/g, "");
+    //.replace(/[&\/\\#,+()$~%.'":*?<>{}0-9\n]/g, ""); //istenmeyen karakterleri kaldır
+    movieTitle = movieTitle.substring(1);
+    movieTitle = movieTitle.substring(0, movieTitle.indexOf("\n")).trim();
 
-    const movieYear = $(
+    //---------------------------
+    let movieYear = $(
       `div[class="titlereference-header"] > div > h3 > span[class="titlereference-title-year"]`
     )
       .text()
-      .trim()
+      //.replace(/[\n\t\r]/g, "");
       .replace(/[^0-9]/g, "");
     //.replace(/[^a-zA-Z0-9 ]/g, "");
 
-    const movieDuration = $(
-      //`div[class="titlereference-header"] > div > ul[class="ipl-inline-list"] > li[class="ipl-inline-list__item"]`
-      `ul.ipl-inline-list:nth-child(8) > li:nth-child(2)`
+    //---------------------------
+    let movieDuration = $(
+      `div[class="titlereference-header"] > div > ul[class="ipl-inline-list"] > li[class="ipl-inline-list__item"]`
+      //`ul.ipl-inline-list:nth-child(8) > li:nth-child(2)`
     )
       .text()
-      .trim();
+      .replace(/[\n\t\r]/g, "");
+    //  .trim()
+    //  .substring(0, 50);
+    //.replace(/[^a-zA-Z0-9]/g, "");
+    if (movieDuration.indexOf("h") && movieDuration.indexOf("min")) {
+      movieDuration = movieDuration
+        .substring(
+          movieDuration.indexOf("h") - 1,
+          movieDuration.indexOf("min") + 3
+        )
+        .trim();
+    } else movieDuration += "N/A";
 
-    const movieSummary = $(
+    //---------------------------
+    //let movieGenre = $();
+
+    //---------------------------
+    let movieAgeRating = $(
+      `div[class="titlereference-header"] > div > ul[class="ipl-inline-list"] > li[class="ipl-inline-list__item"]`
+      //`ul.ipl-inline-list:nth-child(8) > li:nth-child(1)`
+    )
+      .text()
+      .replace(/[\t\r]/g, "");
+    movieAgeRating = movieAgeRating.substring(1);
+    movieAgeRating = movieAgeRating
+      .substring(0, movieAgeRating.indexOf("\n"))
+      .trim();
+    if (movieAgeRating === "") movieAgeRating += "N/A";
+
+    //---------------------------
+    let movieSummary = $(
       //`section[class="titlereference-section-overview"] > div`
       `.titlereference-section-overview > div:nth-child(1)`
     )
       .text()
       .trim();
+    if (movieSummary === "") movieSummary += "N/A";
 
-    const movieRating = $(
+    //---------------------------
+    let movieRating = $(
       `ul[class=ipl-inline-list] > li > div > div > span[class="ipl-rating-star__rating"]`
     )
       .text()
       .trim();
+    if (movieRating === "") movieRating += "N/A";
 
-    const movieRank = $(
+    //---------------------------
+    let movieRank = $(
       `ul[class="ipl-inline-list"] > li[class=ipl-inline-list__item] > a[class=""]`
     )
       .text()
       .trim()
       .replace(/[^0-9]/g, "");
+    if (movieRank === "") movieRank += "N/A";
 
     const moviePoster = $(
       `div[class="titlereference-header"] > div > a > img`
     ).attr("src");
 
-    console.log("Name:", movieTitle);
-    console.log("Year:", movieYear);
+    //---------------------------
+    console.log(
+      "-----------------------------\nMOVIE INFO\n-----------------------------"
+    );
+    console.log("Name: %s (%s)", movieTitle, movieYear);
+    //console.log("Year:", movieYear);
     console.log("Duration:", movieDuration);
+    console.log("Age Rating:", movieAgeRating);
+    //console.log("Age Rating:", movieAgeRating);
     console.log("Summary:", movieSummary);
     console.log("Poster:", moviePoster);
     console.log("Rating:", movieRating);
     console.log("Rank:", movieRank);
+    console.log("-----------------------------");
   } catch (err) {
     console.log("ERROR!");
   }
